8635cd8a2285bd223529963f2317743297c883ef
Hemant Jain <hmtj99@gmail.com>
2021-04-10 02:08:15 +0530

    feat: add email module
    Signed-off-by: Hemant Jain <hmtj99@gmail.com>

---
+++ backend/modules/email.js
@@ -0,0 +1,225 @@
+const nodemailer = require('nodemailer');
+const jwt = require('jsonwebtoken');
+const NEW_PASSWORD_TOKEN_EXPIRY_DURATION = '1h';
+const BASE_URL = 'http://localhost:8080';
+
+const generateTokenFromPasswordHash = (userId,oldPasswordHash,createdAt) => {
+    const secret = oldPasswordHash + '-' + createdAt;
+    const token = jwt.sign({userId},secret,{
+        expiresIn: NEW_PASSWORD_TOKEN_EXPIRY_DURATION,
+    });
+    return token;
+}
+
+const getPasswordResetURL = (user,token) => {
+    return `${BASE_URL}/api/user/reset-password?token=${token}&userid=${user._id}`;
+}
+
+const getEmailTemplate = (user,url) => {
+    const from = process.env.EMAIL_LOGIN
+    const to = user.email
+    const subject = "EasyAuth Password Reset"
+    const html = `
+    <!doctype html>
+    <html lang="en-US">
+
+    <head>
+        <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+        <title>Reset Password</title>
+        <meta name="description" content="Reset Password Email Template.">
+        <style type="text/css">
+            a:hover {text-decoration: underline !important;}
+        </style>
+    </head>
+
+    <body marginheight="0" topmargin="0" marginwidth="0" style="margin: 0px; background-color: #f2f3f8;" leftmargin="0">
+        <!--100% body table-->
+        <table cellspacing="0" border="0" cellpadding="0" width="100%" bgcolor="#f2f3f8"
+            style="@import url(https://fonts.googleapis.com/css?family=Rubik:300,400,500,700|Open+Sans:300,400,600,700); font-family: 'Open Sans', sans-serif;">
+            <tr>
+                <td>
+                    <table style="background-color: #f2f3f8; max-width:670px;  margin:0 auto;" width="100%" border="0"
+                        align="center" cellpadding="0" cellspacing="0">
+                        <tr>
+                            <td style="height:80px;">&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td style="text-align:center;">
+                            <a href="https://github.com/ossdaiict" title="logo" target="_blank">
+                                <img width="60" src="https://avatars3.githubusercontent.com/u/16758973?s=200&v=4" title="logo" alt="logo">
+                            </a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="height:20px;">&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <table width="95%" border="0" align="center" cellpadding="0" cellspacing="0"
+                                    style="max-width:670px;background:#fff; border-radius:3px; text-align:center;-webkit-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);-moz-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);box-shadow:0 6px 18px 0 rgba(0,0,0,.06);">
+                                    <tr>
+                                        <td style="height:40px;">&nbsp;</td>
+                                    </tr>
+                                    <tr>
+                                        <td style="padding:0 35px;">
+                                            <h1 style="color:#1e1e2d; font-weight:500; margin:0;font-size:32px;font-family:'Rubik',sans-serif;">You have
+                                                requested to reset your password</h1>
+                                            <span
+                                                style="display:inline-block; vertical-align:middle; margin:29px 0 26px; border-bottom:1px solid #cecece; width:100px;"></span>
+                                            <p style="color:#455056; font-size:15px;line-height:24px; margin:0;">   
+                                             Hey ${user.email.split('@')[0]},
+                                            </p>
+                                            <p style="color:#455056; font-size:15px;line-height:24px;margin-top:35px;">
+                                                We cannot simply send you your old password. A unique link to reset your
+                                                password has been generated for you. To reset your password, click the
+                                                following link and follow the instructions.                                            
+                                            </p>
+                                            <a href="${url}"
+                                                style="background:#20e277;text-decoration:none !important; font-weight:500; margin-top:35px; color:#fff;text-transform:uppercase; font-size:14px;padding:10px 24px;display:inline-block;border-radius:50px;">Reset
+                                                Password</a>              
+                                            <p style="color:#455056; font-size:15px;line-height:24px; margin-top:35px;">
+                                                If you don’t use this link within 1 hour, it will expire. Hope You have a nice experience.
+                                            </p>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td style="height:40px;">&nbsp;</td>
+                                    </tr>
+                                </table>
+                            </td>
+                        <tr>
+                            <td style="height:20px;">&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td style="text-align:center;">
+                                <p style="font-size:14px; color:rgba(69, 80, 86, 0.7411764705882353); line-height:18px; margin:0 0 0;">&copy; <strong>Powered by EasyAuth, 2021</strong></p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="height:80px;">&nbsp;</td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+        </table>
+        <!--/100% body table-->
+    </body>
+
+    </html>`
+
+    return { from, to, subject, html }
+}
+
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_LOGIN,
+      pass: process.env.EMAIL_PASSWORD
+    }
+});
+
+const getVerificationURL = (hash) => {
+    return `${BASE_URL}/api/user/verify-user?hash=${hash}`;
+}
+
+const getVerificationEmailTemplate = (email,url) => {
+    const from = process.env.EMAIL_LOGIN
+    const to = email
+    const subject = "EasyAuth Account Verification"
+    const html= `
+    <!doctype html>
+    <html lang="en-US">
+
+    <head>
+        <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+        <title>Activate Account</title>
+        <meta name="description" content="Activation Account Link">
+        <style type="text/css">
+            a:hover {text-decoration: underline !important;}
+        </style>
+    </head>
+
+    <body marginheight="0" topmargin="0" marginwidth="0" style="margin: 0px; background-color: #f2f3f8;" leftmargin="0">
+        <!--100% body table-->
+        <table cellspacing="0" border="0" cellpadding="0" width="100%" bgcolor="#f2f3f8"
+            style="@import url(https://fonts.googleapis.com/css?family=Rubik:300,400,500,700|Open+Sans:300,400,600,700); font-family: 'Open Sans', sans-serif;">
+            <tr>
+                <td>
+                    <table style="background-color: #f2f3f8; max-width:670px;  margin:0 auto;" width="100%" border="0"
+                        align="center" cellpadding="0" cellspacing="0">
+                        <tr>
+                            <td style="height:80px;">&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td style="text-align:center;">
+                            <a href="https://github.com/ossdaiict" title="logo" target="_blank">
+                                <img width="60" src="https://avatars3.githubusercontent.com/u/16758973?s=200&v=4" title="logo" alt="logo">
+                            </a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="height:20px;">&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <table width="95%" border="0" align="center" cellpadding="0" cellspacing="0"
+                                    style="max-width:670px;background:#fff; border-radius:3px; text-align:center;-webkit-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);-moz-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);box-shadow:0 6px 18px 0 rgba(0,0,0,.06);">
+                                    <tr>
+                                        <td style="height:40px;">&nbsp;</td>
+                                    </tr>
+                                    <tr>
+                                        <td style="padding:0 35px;">
+                                            <h1 style="color:#1e1e2d; font-weight:500; margin:0;font-size:32px;font-family:'Rubik',sans-serif;">
+                                            Activate your EasyAuth account</h1>
+                                            <span
+                                                style="display:inline-block; vertical-align:middle; margin:29px 0 26px; border-bottom:1px solid #cecece; width:100px;"></span>
+                                            <p style="color:#455056; font-size:15px;line-height:24px; margin:0;">   
+                                             Hey ${email.split('@')[0]},
+                                            </p>
+                                            <p style="color:#455056; font-size:15px;line-height:24px;margin-top:35px;">
+                                                Click the button below to activate your EasyAuth account for event authentication powered by EasyAuth.                                           
+                                            </p>
+                                            <a href="${url}" style="background:#20e277;text-decoration:none !important; font-weight:500; margin-top:35px; color:#fff;text-transform:uppercase; font-size:14px;padding:10px 24px;display:inline-block;border-radius:50px;">
+                                                Activate
+                                            </a>              
+                                            <p style="color:#455056; font-size:15px;line-height:24px; margin-top:35px;">
+                                                Thank You. Hope You have a nice experience.
+                                            </p>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td style="height:40px;">&nbsp;</td>
+                                    </tr>
+                                </table>
+                            </td>
+                        <tr>
+                            <td style="height:20px;">&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td style="text-align:center;">
+                                <p style="font-size:14px; color:rgba(69, 80, 86, 0.7411764705882353); line-height:18px; margin:0 0 0;">&copy; <strong>Powered by EasyAuth, 2021</strong></p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="height:80px;">&nbsp;</td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+        </table>
+        <!--/100% body table-->
+    </body>
+
+    </html>`
+
+    return { from, to, subject, html }
+}
+
+module.exports = {
+    generateTokenFromPasswordHash,
+    getEmailTemplate,
+    getPasswordResetURL,
+    getVerificationURL,
+    getVerificationEmailTemplate,
+    transporter
+}
